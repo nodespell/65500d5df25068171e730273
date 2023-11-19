f2901f9fd1d6230bed9096e7a4f3773418b51aeb
NodeSpell <150180798+nodespell@users.noreply.github.com>
2023-11-19 16:05:10 +0000

    deploy new files

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import { createBrowserRouter, RouterProvider, Routes, Route, Link, Outlet } from
 const router = createBrowserRouter([{ path: "*", Component: Root }]);
 
 function Root() {
-  return <div className="bg-amber-600 w-full h-[100vh]">
-  Deployed to main branch
+  return <div className="bg-amber-600 w-full h-[100vh] whitespace-pre-wrap">
+  Deep into that darkness peering,
+  
+  Long I stood there, wondering, fearing,
+  
+  Doubting, dreaming dreams no mortals
+  
+  Ever dared to dream before;
+  
+  But the silence was unbroken,
+  
+  And the stillness gave no token,
+  
+  And the only word there spoken
+  
+  Was the whispered word, "Lenore!"
+  
+  This I whispered, and an echo
+  
+  Murmured back the word, "Lenore!"
+  
+  Merely this, and nothing more.
 </div>
 }
 
